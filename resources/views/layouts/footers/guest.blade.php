d9700b6a76f95a8dce697e4bfba41e5a98f3e69b
João Carias <joaocariasdefranca@gmail.com>
2019-08-28 19:00:45 -0300

    modificando o layout

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+        <ul>
+            <li>
+            <a href="#">
+                {{ __('Licitações App') }}
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                {{ __('Sobre') }}
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                {{ __('Licença') }}
+            </a>
+            </li>
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>, Desenvolvido por João Teams.
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('index');
 
 Auth::routes();
 
@@ -11,4 +11,44 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/licitacao/index', 'LicitacaoController@index')->name('licitacao');
 Route::get('/licitacao/atualize', 'LicitacaoController@atualize')->name('atualize');
 Route::get('/licitacao/store', 'LicitacaoController@store')->name('store');
-Route::get('/licitacao/licitacoes', 'LicitacaoController@licitacoes')->name('licitacoes');
+Route::get('/licitacao/licitacoes', 'LicitacaoController@licitacoes')->name('licitacoes');Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::get('table-list', function () {
+		return view('pages.table_list');
+	})->name('table');
+
+	Route::get('typography', function () {
+		return view('pages.typography');
+	})->name('typography');
+
+	Route::get('icons', function () {
+		return view('pages.icons');
+	})->name('icons');
+
+	Route::get('map', function () {
+		return view('pages.map');
+	})->name('map');
+
+	Route::get('notifications', function () {
+		return view('pages.notifications');
+	})->name('notifications');
+
+	Route::get('rtl-support', function () {
+		return view('pages.language');
+	})->name('language');
+
+	Route::get('upgrade', function () {
+		return view('pages.upgrade');
+	})->name('upgrade');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+});
+
